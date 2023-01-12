14f7414036208dd42aa5df0802b714301dee722d
Mohammed Al-Rasheed <69544031+MohammedAl-Rasheed@users.noreply.github.com>
2023-01-12 10:38:19 -0500

    done frontend

--- backend/main.py
+++ backend/main.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI
 from fastapi import FastAPI, WebSocket
 from fastapi.responses import HTMLResponse
 from functions.calcs import api_call
+from fastapi.middleware.cors import CORSMiddleware
+
 app = FastAPI()
 
 
@@ -16,7 +18,20 @@ async def get_stock(ticker: str):
 
 @app.get("/{ticker}/{daysTillStrike}/{premium}/{strikePrice}")
 async def get_stock(ticker: str, daysTillStrike: int, premium: float, strikePrice: float):
-    return {"Message: ": api_call(ticker, daysTillStrike, premium, strikePrice)}
+    return {"Message": api_call(ticker, daysTillStrike, premium, strikePrice)}
 
-# Stock Ticker, Strike Price, Premium, men
+
+# allow any request from any origin
+origins = [
+    "*"
+]
+
+# allow any request from any origin
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,31 +8,55 @@ import {
   Code,
   Grid,
   theme,
+  Button,
+  Input,
+  NumberInput,
+  NumberInputField,
 } from "@chakra-ui/react"
 import { ColorModeSwitcher } from "./ColorModeSwitcher"
 import { Logo } from "./Logo"
+import axios from 'axios'
 
-export const App = () => (
-  <ChakraProvider theme={theme}>
-    <Box textAlign="center" fontSize="xl">
-      <Grid minH="100vh" p={3}>
-        <ColorModeSwitcher justifySelf="flex-end" />
-        <VStack spacing={8}>
-          <Logo h="40vmin" pointerEvents="none" />
-          <Text>
-            Edit <Code fontSize="xl">src/App.tsx</Code> and save to reload.
-          </Text>
-          <Link
-            color="teal.500"
-            href="https://chakra-ui.com"
-            fontSize="2xl"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn Chakra
-          </Link>
-        </VStack>
-      </Grid>
-    </Box>
-  </ChakraProvider>
-)
+
+
+export const App = () => {
+
+  const [ticker, setTicker] = React.useState('')
+  const [daysTillStrike, setDaysTillStirke] = React.useState(0)
+  const [premium, setPremium] = React.useState(0)
+  const [strikePrice, setStrikePrice] = React.useState(0)
+  const [profit, setProfit] = React.useState('')
+
+  async function getProfit(ticker: string, daysTillStrike: number, premium: number, strikePrice: number) {
+  
+    axios.get('http://127.0.0.1:8000/' + ticker + '/' + daysTillStrike.toString() + '/' + premium.toString() + '/' + strikePrice.toString())
+    .then((response: { data: any }) => {
+      const data = response.data;
+      const profitd = data['Message'];
+      setProfit(profitd);
+      console.log(data);
+    })
+    .catch((error: any) => {
+      console.log(error);
+    });
+  }
+
+  return (
+    <ChakraProvider theme={theme}>
+      <div className="App">
+          <Input width='auto' min={0} max={100}  type="text" placeholder='Ticker' onChange={(e) => setTicker(e.target.value)} />
+          <NumberInput width='auto' defaultValue={0} onChange={(valueString) => setDaysTillStirke(parseInt(valueString))}>
+            <NumberInputField />
+          </NumberInput>
+          <NumberInput width='auto' defaultValue={0}  onChange={(valueString) => setPremium(parseInt(valueString))}>
+            <NumberInputField />
+          </NumberInput>
+          <NumberInput width='auto' defaultValue={0} onChange={(valueString) => setStrikePrice(parseInt(valueString))}>
+            <NumberInputField />
+          </NumberInput>
+          <Button onClick={() => getProfit(ticker, daysTillStrike, premium, strikePrice)}>Get Data</Button>
+          <p>{profit}</p>
+      </div>
+    </ChakraProvider>
+  )
+}
